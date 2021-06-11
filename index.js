8670ae87172020c7c09aaea8ca751f5c0a87b141
Saeed Khan <saeedkhan@ghost.hsd1.co.comcast.net>
2021-06-11 09:55:21 -0600

    monolith push

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require("express")
 const server = express()
 const cors = require("cors")
 const port = process.env.PORT || 9000
+const path = require("path")
 
 server.use(cors())
 server.use(express.json())
+server.use(express.static(path.join(__dirname, "client/build")))
+
+
 server.use("/api/", (_,res)=>{
     res.json({
         data: "THE API SERVES THEEE DATA"
@@ -15,6 +19,8 @@ server.use("/api/", (_,res)=>{
 server.listen(port, () => {
 console.log(`port available on ${port}`)
 })
+
+
 
 console.log("It is alive!")
 console.log(__dirname)
